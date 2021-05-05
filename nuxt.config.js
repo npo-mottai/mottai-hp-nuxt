409d7acae3fecfd949adb1ceff1ff73876a8dda9
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-05-05 10:02:43 +0900

    feat: set global meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,16 +4,93 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'mottai-hp-nuxt',
+    title: 'NPO法人 MOTTAI',
     htmlAttrs: {
-      lang: 'en',
+      lang: 'ja',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'MOTTAIは「当たり前の裏側にアクセスしやすい社会を創る」をビジョンに掲げて活動するNPO法人です。',
+      },
+      {
+        hid: 'og:site_name',
+        property: 'og:site_name',
+        content: 'NPO法人 MOTTAI | 公式サイト',
+      },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'NPO法人 MOTTAI',
+      },
+      {
+        hid: 'og:type',
+        property: 'og:type',
+        content: 'website',
+      },
+      {
+        hid: 'og:locale',
+        property: 'og:locale',
+        content: 'ja_JP',
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://npo-mottai.org',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          'MOTTAIは「当たり前の裏側にアクセスしやすい社会を創る」をビジョンに掲げて活動するNPO法人です。',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: '/image/ogp.png',
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: 'NPO法人 MOTTAI',
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content:
+          'MOTTAIは「当たり前の裏側にアクセスしやすい社会を創る」をビジョンに掲げて活動するNPO法人です。',
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: '/image/ogp.png',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/image/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        sizes: '32x32',
+        type: 'image/png',
+        href: '/image/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        sizes: '16x16',
+        type: 'image/png',
+        href: '/image/favicon-16x16.png',
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css

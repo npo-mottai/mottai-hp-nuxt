70952445c21d61cf4fe069b67e429645452f9c8b
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-05-05 11:00:23 +0900

    feat: implement contact page

---
+++ pages/contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <div class="container mx-auto px-8 sm:px-16 xl:px-48">
+      <div class="">
+        <h2
+          class="h2--primary-orange text-3xl md:text-4xl mt-6 mb-3 pl-3 font-bold border-l-8"
+        >
+          お問い合わせ
+        </h2>
+        <p class="pb-3 md:text-lg">
+          活動内容についてのご不明点、モッタイナイト・罠オーナー制度・体験事業・各種サポートについてのお問い合わせ、講演依頼、取材の依頼などは、こちらのフォームよりご連絡下さい。
+        </p>
+      </div>
+    </div>
+    <contact-form />
+  </div>
+</template>

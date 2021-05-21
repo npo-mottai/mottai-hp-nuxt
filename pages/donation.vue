df58db73f45956dfabcefec3e4e6d633bfe74030
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2021-05-21 20:35:39 +0900

    chore: add credit card payment info on donation page

--- pages/donation.vue
+++ pages/donation.vue
@@ -8,38 +8,52 @@
         寄付によるサポート
       </h2>
       <p class="pb-3 md:text-lg">
-        NPO法人MOTTAIは「当たり前の裏側にアクセスしやすい社会を創る」をビジョンに掲げて活動しています。そして、当たり前と感じがちな「食べる」という行為について考え直すきっかけを作り、食品ロスをはじめとする様々な社会問題について当事者意識を持つことができる社会を目指しています。寄付をしてくださる方は、下記のフォームにご入力の上、下記口座へのお振込をよろしくお願いします。フォームにご入力いただきましたアドレスにお礼のご挨拶をさせていただきます。
+        NPO法人MOTTAIは「当たり前の裏側にアクセスしやすい社会を創る」をビジョンに掲げて活動しています。そして、当たり前と感じがちな「食べる」という行為について考え直すきっかけを作り、食品ロスをはじめとする様々な社会問題について当事者意識を持つことができる社会を目指しています。寄付をしてくださる方は、下記のフォームにご入力の上、クレジットカードでの寄付、または下記口座へのお振込をよろしくお願いします。フォームにご入力いただきましたアドレスにお礼のご挨拶をさせていただきます。
       </p>
     </div>
+    <hr class="pb-6" />
     <div class="pb-3">
-      <table class="table-fixed">
-        <tbody>
-          <tr>
-            <td class="border px-4 py-2 w-1/3 lg:w-1/4 xl:1/5 w-px-4 py-2">
-              金融機関名
-            </td>
-            <td class="border px-4 py-2">ＰａｙＰａｙ銀行</td>
-          </tr>
-          <tr>
-            <td class="border px-4 py-2">支店名（店番号）</td>
-            <td class="border px-4 py-2">ビジネス営業部（００５）</td>
-          </tr>
-          <tr>
-            <td class="border px-4 py-2">預金種目</td>
-            <td class="border px-4 py-2">普通預金</td>
-          </tr>
-          <tr>
-            <td class="border px-4 py-2">口座番号</td>
-            <td class="border px-4 py-2">４９９９６２１</td>
-          </tr>
-          <tr>
-            <td class="border px-4 py-2">口座名義人（フリガナ）</td>
-            <td class="border px-4 py-2">
-              NPO法人MOTTAI（エヌピーオーホウジンモッタイ）
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="pb-6">
+        <p class="pb-1 md:text-lg">クレジットカードによる寄付</p>
+        <a
+          href="https://syncable.biz/associate/npomottai/donate/"
+          target="_blank"
+          rel="noopener"
+          class="pb-3 md:text-lg text-blue-500 hover:underline"
+          >https://syncable.biz/associate/npomottai/donate/</a
+        >
+      </div>
+      <div class="pb-3">
+        <p class="pb-3 md:text-lg">銀行口座への振り込みによる寄付</p>
+        <table class="table-fixed">
+          <tbody>
+            <tr>
+              <td class="border px-4 py-2 w-1/3 lg:w-1/4 xl:1/5 w-px-4 py-2">
+                金融機関名
+              </td>
+              <td class="border px-4 py-2">ＰａｙＰａｙ銀行</td>
+            </tr>
+            <tr>
+              <td class="border px-4 py-2">支店名（店番号）</td>
+              <td class="border px-4 py-2">ビジネス営業部（００５）</td>
+            </tr>
+            <tr>
+              <td class="border px-4 py-2">預金種目</td>
+              <td class="border px-4 py-2">普通預金</td>
+            </tr>
+            <tr>
+              <td class="border px-4 py-2">口座番号</td>
+              <td class="border px-4 py-2">４９９９６２１</td>
+            </tr>
+            <tr>
+              <td class="border px-4 py-2">口座名義人（フリガナ）</td>
+              <td class="border px-4 py-2">
+                NPO法人MOTTAI（エヌピーオーホウジンモッタイ）
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
     <contact-form />
   </div>
